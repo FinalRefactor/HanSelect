63fec14d4baf5ee4064aa7991822b693c7b2fa31
Ranol <asdhugh1@gmail.com>
2018-06-01 00:40:17 +0900

    Remove useless Util class

--- src/main/java/com/devhn/hrs/ModeDetector.java
+++ src/main/java/com/devhn/hrs/ModeDetector.java
@@ -5,11 +5,9 @@ class ModeDetector {
         ModePlayer mp = new ModePlayer();
 
         if (Static.getString("mode.shuffle").equalsIgnoreCase(mode)) {
-            Util.MODE = HanSelectMode.SHUFFLE;
-            mp.play(Util.MODE);
+            mp.play(HanSelectMode.SHUFFLE);
         } else if (Static.getString("mode.select").equalsIgnoreCase(mode)) {
-            Util.MODE = HanSelectMode.SELECT;
-            mp.play(Util.MODE);
+            mp.play(HanSelectMode.SELECT);
         } else {
             System.out.println(Static.formatError(2, "invalid-keyword"));
         }
--- src/main/java/com/devhn/hrs/Util.java
+++
@@ -1,8 +0,0 @@
-package com.devhn.hrs;
-
-class Util {
-    static HanSelectMode MODE = HanSelectMode.UNKNOWN;
-
-    // todo: make resource bundle
-    static String UNKNOWN_MODE = "Error: #00001 - 키워드가 입력되지 않았습니다.";
-}
