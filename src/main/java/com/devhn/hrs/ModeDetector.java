b91128ccaea99e9c7d1df42406a3eb5073126b76
Ranol <asdhugh1@gmail.com>
2018-06-01 00:38:53 +0900

    Remove useless empty lines

--- src/main/java/com/devhn/hrs/ModeDetector.java
+++ src/main/java/com/devhn/hrs/ModeDetector.java
@@ -7,14 +7,11 @@ class ModeDetector {
         if (Static.getString("mode.shuffle").equalsIgnoreCase(mode)) {
             Util.MODE = HanSelectMode.SHUFFLE;
             mp.play(Util.MODE);
-
         } else if (Static.getString("mode.select").equalsIgnoreCase(mode)) {
             Util.MODE = HanSelectMode.SELECT;
             mp.play(Util.MODE);
-
         } else {
             System.out.println(Static.formatError(2, "invalid-keyword"));
-
         }
     }
 }
